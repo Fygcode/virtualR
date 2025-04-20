2a625300bac187a67359d4768d16d371d16b8e35
Fygcode <muthurajgreat1011@gmail.com>
2025-04-20 21:10:20 +0530

    chnages in brand name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                     <div className="flex items-center flex-shrink-0">
                         <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
                         <span className="text-xl tracking-tight">
-                            VirtualR
+                            VirtualRR
                         </span>
                     </div>
                     <ul className="hidden lg:flex ml-14 space-x-12 ">
